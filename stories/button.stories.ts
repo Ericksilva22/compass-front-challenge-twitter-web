81580f91896fbdf8a7bffea56393656dae655f9d
Erickson <ericksonrodrigues13@gmail.com>
2022-10-31 16:05:33 -0300

    Storybook: create button

--- stories/button.stories.ts
+++ stories/button.stories.ts
@@ -2,25 +2,86 @@
 
 import { Meta, StoryFn } from '@storybook/html';
 
-type ButtonArgs = {
-  primary: boolean;
+export interface ButtonComponentProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large' | 'full';
   label: string;
+  onClick?: (event: Event) => void;
 }
+
+export const buttonComponent = ({
+  primary = true,
+  size = 'large',
+  backgroundColor,
+  label,
+  onClick,
+}: ButtonComponentProps) => {
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.innerText = label;
+
+  if (onClick) button.addEventListener('click', onClick);
+
+  const mode = primary ? 'primary' : 'secondary';
+  button.className = [`${size}`, mode].join(' ');
+
+  if (backgroundColor) button.style.backgroundColor = backgroundColor;
+
+  return `<button type="button" onclick="${onClick ? onClick : ''}" class="${[
+    `${size}`,
+    mode,
+  ].join(' ')}" style="background-color: ${
+    primary ? backgroundColor : 'transparent'
+  }; border-color: ${primary ? 'transparent' : backgroundColor}; color:${
+    primary ? 'white' : backgroundColor
+  }">${label}</button>`;
+};
 
 export default {
-  title: 'button',
-} as Meta<ButtonArgs>;
+  title: 'Example/Button',
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    label: { control: 'text' },
+    onClick: { action: 'onClick' },
+    primary: { control: 'boolean' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+  },
+} as Meta;
 
-export function Story(props:ButtonArgs) {
-    return (
-        ` <button>${props.label}</button>
-        <input>`
-    )
-}
+const Template: StoryFn<ButtonComponentProps> = (args) => {
+  return buttonComponent(args);
+};
 
-export const Primary = Story.bind({});
+export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
   label: 'Button',
 };
 
+export const Secondary = Template.bind({});
+Secondary.args = {
+  primary: false,
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: 'medium',
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
